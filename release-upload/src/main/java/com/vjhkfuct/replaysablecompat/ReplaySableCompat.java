5cf7073c4a65b476b47851b8717a89a3b2ed2474
vjhkfuct <vjhkfuct@163.com>
2026-04-27 23:58:21 +0800

    Add files via upload

---
+++ release-upload/src/main/java/com/vjhkfuct/replaysablecompat/ReplaySableCompat.java
@@ -0,0 +1,14 @@
+package com.vjhkfuct.replaysablecompat;
+
+import net.neoforged.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ReplaySableCompat.MOD_ID)
+public final class ReplaySableCompat {
+    public static final String MOD_ID = "replaysablecompat";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    public ReplaySableCompat() {
+    }
+}
